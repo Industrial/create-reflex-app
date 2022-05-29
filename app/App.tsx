7ef8332de65525f96daefbebb32c853521668a14
Tom Wieland <tom.wieland@gmail.com>
2022-05-29 19:20:24 +0200

    feature(app): try to use useAsync

--- app/App.tsx
+++ app/App.tsx
@@ -1,14 +1,14 @@
-// import { useAsync } from 'react-streaming';
+import { useAsync } from 'react-streaming';
 import React from 'react';
 
 export const App = () => {
   console.log('Rendering App');
 
-  // const movies = useAsync(async () => {
-  //   const response = await fetch('https://catfact.ninja/facts');
-  //   return response.json();
-  // });
-  // console.log('movies', movies);
+  const movies = useAsync(async () => {
+    const response = await fetch('https://catfact.ninja/facts');
+    return response.json();
+  });
+  console.log('movies', movies);
 
   return <h1>These are facts about cats.</h1>;
 };
