541ee51c394fda7d1f1f13f6fbe264cfb8127b25
Tom Wieland <tom.wieland@gmail.com>
2022-06-02 22:02:20 +0200

    refactor(app): use reflex from deno.land/x

--- server.ts
+++ server.ts
@@ -1,26 +1,15 @@
+import { reflexMiddleware } from 'https://deno.land/x/reflex@v0.1.1/mod.ts';
 import { Application } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
 
-import { appSource } from './lib/middleware/appSource.ts';
-import { logger } from './lib/middleware/logger.ts';
-import { serverSideRender } from './lib/middleware/serverSideRender.tsx';
-import { staticFile } from './lib/middleware/staticFile.ts';
-import { vendorSource } from './lib/middleware/vendorSource.ts';
+import { Document } from './app/Document.tsx';
 
-const hostname = Deno.env.get('hostname') ?? '127.0.0.1';
-const port = Number(Deno.env.get('port') ?? 3000);
 const app = new Application();
-const appSourcePrefix = '/.x';
-const vendorSourcePrefix = '/.v';
 
-app.use(logger());
-app.use(vendorSource({ vendorSourcePrefix }));
-app.use(appSource({ appSourcePrefix }));
-app.use(staticFile());
-app.use(serverSideRender({ vendorSourcePrefix }));
-
-console.log(`Listening on http://${hostname}:${port}`);
+app.use(
+  await reflexMiddleware({ Document }),
+);
 
 await app.listen({
-  hostname,
-  port,
+  hostname: Deno.env.get('hostname') ?? '127.0.0.1',
+  port: Number(Deno.env.get('port') ?? 3000),
 });
