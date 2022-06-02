2db5657faaf17bd931fbb7af9d438046ad8e7cbf
Tom Wieland <tom.wieland@gmail.com>
2022-06-02 22:09:23 +0200

    refactor(server): wrote code a bit differently

--- server.ts
+++ server.ts
@@ -3,13 +3,15 @@ import { Application } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
 
 import { Document } from './app/Document.tsx';
 
+const hostname = Deno.env.get('hostname') ?? '127.0.0.1';
+const port = Number(Deno.env.get('port') ?? 3000);
 const app = new Application();
 
-app.use(
-  await reflexMiddleware({ Document }),
-);
+app.use(await reflexMiddleware({ Document }));
+
+console.log(`Listening on http://${hostname}:${port}`);
 
 await app.listen({
-  hostname: Deno.env.get('hostname') ?? '127.0.0.1',
-  port: Number(Deno.env.get('port') ?? 3000),
+  hostname,
+  port,
 });
