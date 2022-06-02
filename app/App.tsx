7b9c88ce6b26ab824334241b4571ddb5e4627071
Tom Wieland <tom.wieland@gmail.com>
2022-06-02 19:22:07 +0200

    refactor(app): Removed unused code

--- app/App.tsx
+++ app/App.tsx
@@ -1,36 +1,6 @@
-// import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import React, { Suspense } from 'react';
 import { Cats } from './Cats.tsx';
 import { ErrorBoundary } from './ErrorBoundary.tsx';
-
-// export const CatFacts = () => {
-//   const { isLoading, error, data } = useQuery('catFacts', async () => {
-//     const result = await fetch('https://catfact.ninja/cats');
-//     const resultJSON = await result.json();
-//     return resultJSON;
-//   });
-//   if (isLoading) {
-//     return <div>Loading...</div>;
-//   }
-//   if (error) {
-//     return <div>Error!</div>;
-//   }
-//   return (
-//     <div>
-//       <h1>{data.name}</h1>
-//       <p>{data.description}</p>
-//       <p>{data.html_url}</p>
-//     </div>
-//   );
-// };
-
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       suspense: true,
-//     },
-//   },
-// });
 
 export const App = () => {
   return (
@@ -44,16 +14,4 @@ export const App = () => {
       </Suspense>
     </div>
   );
-
-  // return (
-  //   <QueryClientProvider client={queryClient}>
-  //     <div>
-  //       <h1>Application</h1>
-  //       <p>These are facts about cats:</p>
-  //       <Suspense fallback={<h1>Loading...</h1>}>
-  //         <CatFacts />
-  //       </Suspense>
-  //     </div>
-  //   </QueryClientProvider>
-  // );
 };
