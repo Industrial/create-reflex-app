7e04ea3f563025ad114d9f8cceef8936752cc8a9
Tom Wieland <tom.wieland@gmail.com>
2022-06-03 13:05:23 +0200

    chore(build): bump version of reflex

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { reflexMiddleware } from 'https://deno.land/x/reflex@v0.1.1/mod.ts';
+import { reflexMiddleware } from 'https://deno.land/x/reflex@v0.3.0/mod.ts';
 import { Application } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
 
 import { Document } from './app/Document.tsx';
